3dc4865695a99f2df78602d630ef58c342e8895a
ptanh2k <pta230500x090800@gmail.com>
2022-10-13 09:46:04 +0700

    Add authorization for certain features

--- sample.sql
+++ sample.sql
@@ -1,26 +1,24 @@
 CREATE TABLE IF NOT EXISTS servers (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(20),
-    ip TEXT NOT NULL,
-    port SMALLINT NOT NULL,
-    status BOOLEAN NOT NULL
+    server_id SERIAL PRIMARY KEY,
+    server_name VARCHAR(20) UNIQUE NOT NULL,
+    status BOOLEAN NOT NULL,
+	created_time TIMESTAMP NOT NULL,
+	last_updated TIMESTAMP NOT NULL,
+	ipv4 VARCHAR(50) UNIQUE NOT NULL,
+	user_id INTEGER REFERENCES users(id)
 );
 
-
-INSERT INTO servers (name, ip, port, status) VALUES
-('server1', '192.168.1.0', 8080, 'on'),
-('server2', '192.168.1.1', 3000, 'off')
+INSERT INTO servers (server_name, status, created_time, last_updated, ipv4, user_id) VALUES
+('Cycir', 'ON', LOCALTIMESTAMP, LOCALTIMESTAMP, '192.168.1.1', 1)
 
 SELECT * FROM servers
 
-DROP TABLE users
+-- DROP TABLE users
 
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    created_at TIME,
-    updated_at TIME,
 );
 
-SELECT * FROM users WHERE users.username = 'admin' AND users.deleted_at IS NULL LIMIT 1
+SELECT * FROM users WHERE users.username = 'admin'
